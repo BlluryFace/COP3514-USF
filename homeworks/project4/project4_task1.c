730e2781c65100aaa4734f6cf3d5ca072afcc742
Huy Bui <huyvijamco@gmail.com>
2024-10-21 17:32:03 -0400

    Finish project4_task1.c

--- homeworks/project4/project4_task1.c
+++ homeworks/project4/project4_task1.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+/// Calculate the score of each person to see who win the round.
+/// A turn of the game consists of either picking the first or the last number in the array
+/// The winner is the player with the highest sum for the picked numbers
+/// Both players are using the same strategy, picking the largest number available remaining in the array
+/// \param board: array of numbers with the pointer pointing to the first variable
+/// \param n: number of elements in the board
 void roundResult(int *board, int n) {
     int *pointer1, *pointer2;
     int Marijorie = 0;
     int Jonh = 0;
-    char player = 'Marijorie';
-    *pointer1 = *board;
-    *pointer2 = *board + n-1;
-    for (; *pointer1 != pointer2;) {
-        if (player == 'Marijorie') {
+    char player = 'M';
+    pointer1 = board;
+    pointer2 = board + n-1;
+    // Comparing the value of the variable at the head and the tail to see which is higher
+    // Player in the turn increment their score by the value
+    // The turn then switched to the other person
+    for (; pointer1 <= pointer2;) {
+        if (player == 'M') {
             {
                 int headVal = *pointer1;
                 int tailVal = *pointer2;
@@ -19,8 +28,8 @@ void roundResult(int *board, int n) {
                     pointer1++;
                 }
             }
-            player = 'Jonh';
-        } else {
+            player = 'J';
+        } else if (player = 'J') {
                 int headVal = *pointer1;
                 int tailVal = *pointer2;
                 if (headVal < tailVal) {
@@ -30,25 +39,31 @@ void roundResult(int *board, int n) {
                     Jonh += headVal;
                     pointer1++;
                 }
-            player = 'Marijorie';
+            player = 'M';
         }
     }
     if (Marijorie > Jonh) {
-        printf("Marijorie");
+        printf("Marijorie\n");
+    } else if(Marijorie < Jonh) {
+        printf("Jonh\n");
     } else {
-        printf("Jonh");
+        printf("Draw\n");
     }
 }
 int main() {
     int T = 0;
     int N = 0;
     int *arrayPointer;
-    scanf("%d", &T);
-    scanf("%d", &N);
-    int board[N];
-    int temp = 0;
-    //TODO: Implement this array with the function
-    for (arrayPointer = board; arrayPointer < board + N; arrayPointer++)
-        scanf("%d", arrayPointer);
+    scanf("%d", &T); // number of rounds in an array
+    for (; T > 0; T--) {
+        scanf("%d", &N); //number of elements in an array
+        int board[N];
+        // Pointer is pointing to an index in an array
+        // scanf function will assign a number the pointer
+        // Therefore, updating the value at that index
+        for (arrayPointer = board; arrayPointer < board + N; arrayPointer++)
+            scanf("%d", arrayPointer);
+        roundResult(board, N);
+    }
     return 0;
 }

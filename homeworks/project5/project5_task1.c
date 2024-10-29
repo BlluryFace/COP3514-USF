aa1575694c125e5ae7cd8bdd429e3af86412d823
Huy Bui <huyvijamco@gmail.com>
2024-10-28 22:20:21 -0400

    Added documentation for project5_task1.c

--- homeworks/project5/project5_task1.c
+++ homeworks/project5/project5_task1.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
-
+/// Reading user's input through the terminal
+/// \param str: the character array where the user want to store the input
 void readLines(char *str) {
     int ch;
     while((ch = getchar()) == ' ' || ch == '\t') {
+        // Skipping all white spaces in front of the input
     }
-    if (ch != EOF){
+    // Store the first non-whitespace character
+    *str = ch;
+    str++;
+    // Store the rest of the input into the array until reaching the new line character
+    while ((ch = getchar()) != '\n'&& ch != EOF){
         *str = ch;
         str++;
     }
-
-    while ((ch = getchar()) != '\n' && ch != EOF){
-        *str = ch;
-        str++;
-    }
-    *str = '\0';
+    *str = '\0'; // Adding the end of string character
 }
-
+///
+/// \param input: the sentence that needs to be split
+/// \param word1: the character array where user want to store the first word
+/// \param word2: the character array where user want to store the second word
 void split (char *input,  char *word1, char *word2){
-    for (; *input == ' ' || *input == '\t'; input++){
-    }
     for (; *input != ' ' && *input != '\t' && *input != '\0'; input++){
         *word1 = *input;
         word1++;
     }
-    *word1 = '\0';
+    *word1 = '\0'; // Adding the end of string character
+
     for (; *input == ' ' || *input == '\t'; input++){
+        // Skipping all white spaces in between the words
     }
 
     for (; *input != ' ' && *input != '\t' && *input != '\0'; input++){
         *word2 = *input;
         word2++;
     }
-    *word2 = '\0';
+    *word2 = '\0'; // Adding the end of string character
 }
 
 int main(){
